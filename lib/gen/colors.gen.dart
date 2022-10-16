d859e7a7e7c78d9249d729e12c939b290b301891
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-10-16 09:42:08 +0100

    style: :art: add app colors

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,105 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  /// Color: #282A3A
+  static const Color black = Color(0xFF282A3A);
+
+  /// Color: #0062FF
+  static const Color blue = Color(0xFF0062FF);
+
+  /// Color: #F3F4F6
+  static const Color gray100 = Color(0xFFF3F4F6);
+
+  /// Color: #E5E7EB
+  static const Color gray200 = Color(0xFFE5E7EB);
+
+  /// Color: #D1D5DB
+  static const Color gray300 = Color(0xFFD1D5DB);
+
+  /// Color: #9CA3AF
+  static const Color gray400 = Color(0xFF9CA3AF);
+
+  /// Color: #FCFCFD
+  static const Color gray50 = Color(0xFFFCFCFD);
+
+  /// Color: #6B7280
+  static const Color gray500 = Color(0xFF6B7280);
+
+  /// Color: #4B5563
+  static const Color gray600 = Color(0xFF4B5563);
+
+  /// Color: #374151
+  static const Color gray700 = Color(0xFF374151);
+
+  /// Color: #1F2937
+  static const Color gray800 = Color(0xFF1F2937);
+
+  /// Color: #0F172A
+  static const Color gray900 = Color(0xFF0F172A);
+
+  /// Color: #FE964A
+  static const Color orange = Color(0xFFFE964A);
+
+  /// MaterialColor:
+  ///   50: #FFE8E7FF
+  ///   100: #FFC6C3FF
+  ///   200: #FFA09CFF
+  ///   300: #FF7974FF
+  ///   400: #FF5D56FF
+  ///   500: #FF4038FF
+  ///   600: #FF3A32FF
+  ///   700: #FF322BFF
+  ///   800: #FF2A24FF
+  ///   900: #FF1C17FF
+  static const MaterialColor primary = MaterialColor(
+    0xFF4038FF,
+    <int, Color>{
+      50: Color(0xFFE8E7FF),
+      100: Color(0xFFC6C3FF),
+      200: Color(0xFFA09CFF),
+      300: Color(0xFF7974FF),
+      400: Color(0xFF5D56FF),
+      500: Color(0xFF4038FF),
+      600: Color(0xFF3A32FF),
+      700: Color(0xFF322BFF),
+      800: Color(0xFF2A24FF),
+      900: Color(0xFF1C17FF),
+    },
+  );
+
+  /// Color: #8C62FF
+  static const Color purple = Color(0xFF8C62FF);
+
+  /// Color: #0F172A
+  static const Color secondary = Color(0xFF0F172A);
+
+  /// Color: #CBD5E1
+  static const Color secondary100 = Color(0xFFCBD5E1);
+
+  /// Color: #64748B
+  static const Color secondary200 = Color(0xFF64748B);
+
+  /// Color: #334155
+  static const Color secondary300 = Color(0xFF334155);
+
+  /// Color: #1E293B
+  static const Color secondary400 = Color(0xFF1E293B);
+
+  /// Color: #F1F5F9
+  static const Color secondary50 = Color(0xFFF1F5F9);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
