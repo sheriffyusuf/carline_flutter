accb28ab20f84a3412bf52b4393e47153cbfb595
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-11-30 21:19:19 -0100

    feat: :sparkles: create car card widget

---
+++ lib/pages/home/widgets/car_card.dart
@@ -0,0 +1,88 @@
+import 'package:carline_flutter/gen/assets.gen.dart';
+import 'package:carline_flutter/gen/colors.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:tabler_icons/tabler_icons.dart';
+
+class CarCard extends StatelessWidget {
+  const CarCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 228,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: AppColor.gray100, borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              Assets.svgs.audiLogo.svg(),
+              const Spacer(),
+              const Icon(
+                Icons.favorite,
+                color: AppColor.gray300,
+              )
+            ],
+          ),
+          16.height,
+          Assets.images.audi.image(),
+          8.height,
+          Row(
+            children: [
+              Text(
+                'Audi A8 Quattro',
+                style: context.textTheme.caption,
+              ),
+              const Spacer(),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.star,
+                    size: 16,
+                    color: Color(0xFFFACC15),
+                  ),
+                  4.width,
+                  Text(
+                    '4.8',
+                    style: context.textTheme.overline,
+                  )
+                ],
+              ),
+            ],
+          ),
+          9.height,
+          const Divider(),
+          9.height,
+          Row(
+            children: [
+              Row(
+                children: [
+                  const Icon(
+                    TablerIcons.manual_gearbox,
+                    size: 14.0,
+                    color: AppColor.gray500,
+                  ),
+                  4.width,
+                  Text(
+                    'Automatic',
+                    style: context.textTheme.overline,
+                  )
+                ],
+              ),
+              const Spacer(),
+              Text(
+                '\$112,150.00',
+                style: context.textTheme.caption!
+                    .copyWith(color: AppColor.primary),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
